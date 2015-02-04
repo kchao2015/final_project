6db141f525cf88780764c8934bd6799a0c6a3fa6
kchao2015 <kchao21@gmail.com>
2015-02-04 16:46:56 -0600

    prelim week 4 hw

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,26 @@
+class TripsController < ApplicationController
+  
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find_by(id: params["id"])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
