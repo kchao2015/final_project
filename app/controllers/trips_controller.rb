bdcfad6eb389bb3cb0ea972972d55848bbd8c7ee
kchao2015 <kchao21@gmail.com>
2015-02-12 18:21:46 -0600

    updated golden seven actions

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,18 +11,30 @@ class TripsController < ApplicationController
   end
 
   def new
+    @trip = Trip.new
   end
 
   def create
+    trip_params = params.require(:trip).permit(:name, :duration, :season)
+    Trip.create(trip_params)
+    redirect_to trips_path
   end
 
   def edit
+    @trip = Trip.find_by(id: params["id"])
   end
 
   def update
+    trip_params = params.require(:trip).permit(:name, :duration, :season)
+    @trip = Trip.find_by(id: params["id"])
+    @trip.update(trip_params)
+    redirect_to trips_path
   end
 
   def destroy
+    @trip = Trip.find_by(id: params["id"])
+    @trip.destroy
+    redirect_to trips_path
   end
 
 end
